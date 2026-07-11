9ea28a1e2c86ee425c08b0082cce14119175e358
jabeztay <jabeztay@gmail.com>
2026-07-12 07:56:11 +0800

    ch1/ch2 storage/caching

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +34,86 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	mType, _, err := mime.ParseMediaType(mediaType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to map file type", err)
+		return
+	}
+
+	if mType != "image/jpeg" && mType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "image can only be jpeg or png", err)
+		return
+	}
+
+	fileExtension, err := mime.ExtensionsByType(mType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to map file type", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	fileNameStr := base64.RawURLEncoding.EncodeToString(key)
+	fileName := fmt.Sprintf("%s%s", fileNameStr, fileExtension[0])
+
+	f, err := os.Create(filepath.Join(cfg.assetsRoot, fileName))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating local file", err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error writing to local file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error fetching video info", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", err)
+		return
+	}
+
+	b, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read file", err)
+		return
+	}
+
+	t := thumbnail{
+		data:      b,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoID] = t
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, fileName)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
